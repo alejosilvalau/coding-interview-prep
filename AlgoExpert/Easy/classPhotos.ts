6cacca25000b009f23192678e7868e4463e12ad7
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-08-02 11:30:58 -0300

    Finished "Class Photos" question

---
+++ AlgoExpert/Easy/classPhotos.ts
@@ -0,0 +1,19 @@
+export function classPhotos(
+	redShirtHeights: number[],
+	blueShirtHeights: number[]
+) {
+	redShirtHeights.sort((a, b) => b - a);
+	blueShirtHeights.sort((a, b) => b - a);
+
+	const shirtColorInFirstRow =
+		redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE";
+	for (let idx = 0; idx < redShirtHeights.length; idx++) {
+		const redShirtHeight = redShirtHeights[idx];
+		const blueShirtHeight = blueShirtHeights[idx];
+
+		if (shirtColorInFirstRow === "RED") {
+			if (redShirtHeight >= blueShirtHeight) return false;
+		} else if (blueShirtHeight >= redShirtHeight) return false;
+	}
+	return true;
+}
