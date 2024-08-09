540ddcea93bf5dea8c2f5b02c0715fdee2463dc4
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-08-09 07:11:37 -0300

    Solved "Binary Search" algorithm question with both recursive and iterative solutions

---
+++ AlgoExpert/Easy/binarySearch-sol2.ts
@@ -0,0 +1,23 @@
+export function binarySearch(array: number[], target: number): number {
+	return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+function binarySearchHelper(
+	array: number[],
+	target: number,
+	left: number,
+	right: number
+): number {
+	while (left <= right) {
+		const middle = Math.floor((left + right) / 2);
+		const potentialMatch = array[middle];
+		if (target === potentialMatch) {
+			return middle;
+		} else if (target < potentialMatch) {
+			right = middle - 1;
+		} else {
+			left = middle + 1;
+		}
+	}
+	return -1;
+}
