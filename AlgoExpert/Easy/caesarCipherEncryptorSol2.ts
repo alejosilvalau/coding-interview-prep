50bdb82ddf5fe3654b414fe300fa1dfc1c0b1b64
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-08-16 06:30:27 -0300

    Solved "caesar-cipher-encryptor" question

---
+++ AlgoExpert/Easy/caesarCipherEncryptorSol2.ts
@@ -0,0 +1,14 @@
+export function caesarCipherEncryptor(string: string, key: number) {
+	const newLetters: string[] = [];
+	const newKey = key % 26;
+	const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+	for (const letter of string) {
+		newLetters.push(getNewLetter(letter, newKey, alphabet));
+	}
+	return newLetters.join("");
+}
+
+function getNewLetter(letter: string, key: number, alphabet: string[]) {
+	const newLetterCode = alphabet.indexOf(letter) + key;
+	return alphabet[newLetterCode % 26];
+}
