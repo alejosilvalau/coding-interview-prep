add8c81b3a5f321b4d1b6ace5796abaa72b99483
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2024-09-06 07:24:47 -0300

    Finished bst-traversal

---
+++ AlgoExpert/Medium/BSTTraversal.ts
@@ -0,0 +1,41 @@
+// O(n) time | O(n) space
+// n = number of nodes in the tree
+
+class BST {
+	value: number;
+	left: BST | null;
+	right: BST | null;
+
+	constructor(value: number) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+export function inOrderTraverse(tree: BST | null, array: number[]) {
+	if (tree !== null) {
+		inOrderTraverse(tree.left, array);
+		array.push(tree.value);
+		inOrderTraverse(tree.right, array);
+	}
+	return array;
+}
+
+export function preOrderTraverse(tree: BST | null, array: number[]) {
+	if (tree !== null) {
+		array.push(tree.value);
+		preOrderTraverse(tree.left, array);
+		preOrderTraverse(tree.right, array);
+	}
+	return array;
+}
+
+export function postOrderTraverse(tree: BST | null, array: number[]) {
+	if (tree !== null) {
+		postOrderTraverse(tree.left, array);
+		postOrderTraverse(tree.right, array);
+		array.push(tree.value);
+	}
+	return array;
+}
