3a8e8e00f5a43356a433a116b2aa0149c4b6e4f9
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-01-19 09:14:24 -0300

    First solution of making wordle on react

--- FrontendExpert/React Components/wordle-sol1.jsx
+++ FrontendExpert/React Components/wordle-sol1.jsx
@@ -14,8 +14,8 @@ export default function Wordle() {
 			const response = await fetch(WORD_LIST_API_URL);
 			const words = await response.json();
 			setSolution(
-				words[Math.floor(Math.random() * words.length)]
-			).toLowerCase();
+				words[Math.floor(Math.random() * words.length)].toLowerCase()
+			);
 		};
 
 		fetchSolution();
@@ -24,9 +24,10 @@ export default function Wordle() {
 	useEffect(() => {
 		if (solution == null) return;
 
-		const onPressKey = (event) => {
-			if (guesses[NUM_GUESSES - 1] != null || guesses.includes(solution))
+		const onPressKey = event => {
+			if (guesses[NUM_GUESSES - 1] != null || guesses.includes(solution)) {
 				return;
+			}
 
 			const charCode = event.key.toLowerCase().charCodeAt(0);
 			const isLetter =
@@ -34,12 +35,12 @@ export default function Wordle() {
 				charCode >= "a".charCodeAt(0) &&
 				charCode <= "z".charCodeAt(0);
 
-			setCurrentGuess((prevGuess) => {
+			setCurrentGuess(prevGuess => {
 				if (event.key === "Backspace") {
 					return prevGuess.slice(0, -1);
 				} else if (event.key === "Enter" && prevGuess.length === WORD_LENGTH) {
-					const currentGuessIndex = guesses.findIndex((guess) => guess == null);
-					const guesssesClone = [...guesses];
+					const currentGuessIndex = guesses.findIndex(guess => guess == null);
+					const guessesClone = [...guesses];
 					guessesClone[currentGuessIndex] = prevGuess;
 					setGuesses(guessesClone);
 					return "";
@@ -54,7 +55,7 @@ export default function Wordle() {
 		return () => window.removeEventListener("keydown", onPressKey);
 	}, [guesses, solution]);
 
-	const currentGuessIndex = guesses.findIndex((guess) => guess == null);
+	const currentGuessIndex = guesses.findIndex(guess => guess == null);
 
 	if (solution == null) return null;
 
@@ -64,12 +65,12 @@ export default function Wordle() {
 				return (
 					<GuessLine
 						key={index}
-						guess={(i === currentGuessIndex
+						guess={(index === currentGuessIndex
 							? currentGuess
 							: guess ?? ""
 						).padEnd(WORD_LENGTH)}
 						solution={solution}
-						isFinal={currentGuessIndex > i || currentGuessIndex === -1}
+						isFinal={currentGuessIndex > index || currentGuessIndex === -1}
 					/>
 				);
 			})}
@@ -81,15 +82,15 @@ function GuessLine({ guess, solution, isFinal }) {
 	return (
 		<div className='line'>
 			{guess.split("").map((char, index) => {
-				let className = "title";
+				let className = "tile";
 
 				if (isFinal) {
 					if (char === solution[index]) {
-						className += "correct";
+						className += " correct";
 					} else if (solution.includes(char)) {
-						className += "close";
+						className += " close";
 					} else {
-						className += "incorrect";
+						className += " incorrect";
 					}
 				}
 
