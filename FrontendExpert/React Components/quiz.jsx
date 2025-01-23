e53da8f8471bdad5865499dcde0ba63f053dbd0b
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-01-23 08:41:24 -0300

    Changed tabs to spaces

--- FrontendExpert/React Components/quiz.jsx
+++ FrontendExpert/React Components/quiz.jsx
@@ -3,71 +3,71 @@ import React, { useEffect, useState } from "react";
 const QUIZ_API_BASE_URL = "https://api.frontendexpert.io/api/fe/quiz";
 
 export default function Quiz() {
-	const [questions, setQuestions] = useState(null);
-	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-	const [chosenAnswers, setChosenAnswers] = useState([]);
+  const [questions, setQuestions] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [chosenAnswers, setChosenAnswers] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch(QUIZ_API_BASE_URL);
-			const jsonResponse = await response.json();
-			setQuestions(jsonResponse);
-		};
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(QUIZ_API_BASE_URL);
+      const jsonResponse = await response.json();
+      setQuestions(jsonResponse);
+    };
 
-		fetchData();
-	}, []);
+    fetchData();
+  }, []);
 
-	if (questions == null) return null;
+  if (questions == null) return null;
 
-	const updateChosenAnswers = (questionIndex, answerIndex) => {
-		const newChosenAnswers = [...chosenAnswers];
-		newChosenAnswers[questionIndex] = answerIndex;
-		setChosenAnswers(newChosenAnswers);
-	};
+  const updateChosenAnswers = (questionIndex, answerIndex) => {
+    const newChosenAnswers = [...chosenAnswers];
+    newChosenAnswers[questionIndex] = answerIndex;
+    setChosenAnswers(newChosenAnswers);
+  };
 
-	const currentQuestion = questions[currentQuestionIndex];
-	const isFirstQuestion = currentQuestionIndex === 0;
-	const isLastQuestion = currentQuestionIndex === questions.length - 1;
+  const currentQuestion = questions[currentQuestionIndex];
+  const isFirstQuestion = currentQuestionIndex === 0;
+  const isLastQuestion = currentQuestionIndex === questions.length - 1;
 
-	return (
-		<>
-			<h1>{currentQuestion.question}</h1>
-			{currentQuestion.answers.map((answer, answerIndex) => {
-				const chosenAnswer = chosenAnswers[currentQuestionIndex];
-				let className = "answer";
+  return (
+    <>
+      <h1>{currentQuestion.question}</h1>
+      {currentQuestion.answers.map((answer, answerIndex) => {
+        const chosenAnswer = chosenAnswers[currentQuestionIndex];
+        let className = "answer";
 
-				if (chosenAnswer === answerIndex) {
-					className +=
-						currentQuestion.correctAnswer === chosenAnswer
-							? " correct"
-							: " incorrect";
-				}
+        if (chosenAnswer === answerIndex) {
+          className +=
+            currentQuestion.correctAnswer === chosenAnswer
+              ? " correct"
+              : " incorrect";
+        }
 
-				return (
-					<h2
-						key={answer}
-						className={className}
-						onClick={() => {
-							if (chosenAnswer != null) return;
-							updateChosenAnswers(currentQuestionIndex, answerIndex);
-						}}
-					>
-						{answer}
-					</h2>
-				);
-			})}
-			<button
-				disabled={isFirstQuestion}
-				onClick={() => setCurrentQuestionIndex(currentQuestionIndex - 1)}
-			>
-				Back
-			</button>
-			<button
-				disabled={isLastQuestion || chosenAnswers[currentQuestionIndex] == null}
-				onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
-			>
-				Next
-			</button>
-		</>
-	);
+        return (
+          <h2
+            key={answer}
+            className={className}
+            onClick={() => {
+              if (chosenAnswer != null) return;
+              updateChosenAnswers(currentQuestionIndex, answerIndex);
+            }}
+          >
+            {answer}
+          </h2>
+        );
+      })}
+      <button
+        disabled={isFirstQuestion}
+        onClick={() => setCurrentQuestionIndex(currentQuestionIndex - 1)}
+      >
+        Back
+      </button>
+      <button
+        disabled={isLastQuestion || chosenAnswers[currentQuestionIndex] == null}
+        onClick={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
+      >
+        Next
+      </button>
+    </>
+  );
 }
