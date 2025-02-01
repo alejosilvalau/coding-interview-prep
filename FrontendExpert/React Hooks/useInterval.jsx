4cfe55d4439b49adde41d54c5d2d932e77306ee4
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-02-01 09:25:22 -0300

    Finished react custom hook useInterval

---
+++ FrontendExpert/React Hooks/useInterval.jsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from "react";
+
+function useInterval(callback, delay) {
+  const callbackRef = useRef();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay == null) return;
+
+    const intervalID = setInterval(() => callbackRef.current(), delay);
+    return () => clearInterval(intervalID);
+  }, [delay]);
+}
+
+exports.useInterval = useInterval;
