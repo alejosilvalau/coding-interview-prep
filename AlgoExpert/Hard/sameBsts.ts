82dd129b4fa291256705bd2caea351c7588b6b52
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-02-22 08:12:43 -0300

    Finished same BSTs problem

---
+++ AlgoExpert/Hard/sameBsts.ts
@@ -0,0 +1,41 @@
+export function sameBsts(arrayOne: number[], arrayTwo: number[]) {
+  return areSameBsts(arrayOne, arrayTwo, 0, 0, -Infinity, Infinity);
+}
+
+function areSameBsts(
+  arrayOne: number[],
+  arrayTwo: number[],
+  rootIdxOne: number,
+  rootIdxTwo: number,
+  minVal: number,
+  maxVal: number
+): boolean {
+  if (rootIdxOne === -1 || rootIdxTwo === -1) return rootIdxOne === rootIdxTwo;
+  if (arrayOne.length !== arrayTwo.length) return false;
+  if (arrayOne[rootIdxOne] !== arrayTwo[rootIdxTwo]) return false;
+
+  const leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal);
+  const leftRootIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal);
+  const rightRootIdxOne = getIdxOfFirstBiggerOrEqual(arrayOne, rootIdxOne, maxVal);
+  const rightRootIdxTwo = getIdxOfFirstBiggerOrEqual(arrayTwo, rootIdxTwo, maxVal);
+
+  const currentValue = arrayOne[rootIdxOne];
+  const leftAreTheSame = areSameBsts(arrayOne, arrayTwo, leftRootIdxOne, leftRootIdxTwo, minVal, currentValue);
+  const rightAreTheSame = areSameBsts(arrayOne, arrayTwo, rightRootIdxOne, rightRootIdxTwo, currentValue, maxVal);
+
+  return leftAreTheSame && rightAreTheSame;
+}
+
+function getIdxOfFirstSmaller(array: number[], startingIdx: number, minVal: number) {
+  for (let i = startingIdx + 1; i < array.length; i++) {
+    if (array[i] < array[startingIdx] && array[i] >= minVal) return i;
+  }
+  return -1;
+}
+
+function getIdxOfFirstBiggerOrEqual(array: number[], startingIdx: number, maxVal: number) {
+  for (let i = startingIdx + 1; i < array.length; i++) {
+    if (array[i] >= array[startingIdx] && array[i] < maxVal) return i;
+  }
+  return -1;
+}
