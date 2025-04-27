b798785b3a1c69292a795f77216491dbade6041c
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-04-27 10:43:04 -0300

    Finished "Sliding Window Maximum" problem

--- LeetCode/Sliding Window/sliding-window-maximum.ts
+++ LeetCode/Sliding Window/sliding-window-maximum.ts
@@ -1,1 +1,57 @@
-console.log('hello world');
+// O(n) time | O(n) space
+// n == length of the nums array
+//
+// Even if the shift operation has an O(n) time complexity,
+// it can only happen once per element. This means
+// O(n + n) == O(2n) ~= O(n) time complexity.
+//
+// An actual deque implementation would be more efficient
+function maxSlidingWindow(nums: number[], k: number): number[] {
+  const n = nums.length;
+  const res: number[] = new Array(n - k + 1);
+  const deque: number[] = [];
+  let left = 0,
+    right = 0;
+
+  while (right < n) {
+    console.log(`ALGORITHM STATE > left: ${left}, right: ${right}, deque: ${deque}, res: ${res}`);
+    // Keep deque in decreasing order
+    while (deque.length && nums[deque[deque.length - 1]] < nums[right]) {
+      deque.pop();
+    }
+    deque.push(right);
+
+    // Remove elements outside the window
+    if (left > deque[0]) {
+      deque.shift();
+    }
+
+    // Add result when we have a full window
+    if (right + 1 >= k) {
+      res[left] = nums[deque[0]];
+      left++;
+    }
+    right++;
+  }
+  console.log(`ALGORITHM STATE > left: ${left}, right: ${right}, deque: ${deque}, res: ${res}`);
+  return res;
+}
+
+export default maxSlidingWindow;
+
+//Local test cases:
+/*
+ALGORITHM STATE > left: 0, right: 0, deque: , res: ,,,,,
+ALGORITHM STATE > left: 0, right: 1, deque: 0, res: ,,,,,
+ALGORITHM STATE > left: 0, right: 2, deque: 1, res: ,,,,,
+ALGORITHM STATE > left: 1, right: 3, deque: 1,2, res: 3,,,,,
+ALGORITHM STATE > left: 2, right: 4, deque: 1,2,3, res: 3,3,,,,
+ALGORITHM STATE > left: 3, right: 5, deque: 4, res: 3,3,5,,,
+ALGORITHM STATE > left: 4, right: 6, deque: 4,5, res: 3,3,5,5,,
+ALGORITHM STATE > left: 5, right: 7, deque: 6, res: 3,3,5,5,6,
+ALGORITHM STATE > left: 6, right: 8, deque: 7, res: 3,3,5,5,6,7
+Input: s="1,3,-1,-3,5,3,6,7", t="3"
+Output: "3,3,5,5,6,7"
+Expected: "3,3,5,5,6,7"
+Test PASSED
+*/
--- local-testing-suite.ts
+++ local-testing-suite.ts
@@ -1,21 +1,13 @@
-// import { minWindow } from './minWindow';
-
-// function minWindow(s: string, t: string): string {
-
-// }
+import maxSlidingWindow from './LeetCode/Sliding Window/sliding-window-maximum';
 
 // Test cases
-const testCases = [
-  { s: 'ADOBECODEBANC', t: 'ABC', expected: 'BANC' },
-  { s: 'a', t: 'a', expected: 'a' },
-  { s: 'a', t: 'aa', expected: '' },
-];
+const testCases = [{ s: [1, 3, -1, -3, 5, 3, 6, 7], k: 3, expected: [3, 3, 5, 5, 6, 7] }];
 
 testCases.forEach(test => {
-  const result = minWindow(test.s, test.t);
-  console.log(`Input: s="${test.s}", t="${test.t}"`);
+  const result = maxSlidingWindow(test.s, test.k);
+  console.log(`Input: s="${test.s}", t="${test.k}"`);
   console.log(`Output: "${result}"`);
   console.log(`Expected: "${test.expected}"`);
-  console.log(`Test ${result === test.expected ? 'PASSED' : 'FAILED'}`);
+  console.log(`Test ${JSON.stringify(result) === JSON.stringify(test.expected) ? 'PASSED' : 'FAILED'}`);
   console.log('-------------------');
 });
