c83f98f73ee099a8d8d4551f259b9512958ca7f6
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-04-30 06:36:27 -0300

    Finished "Linked List Cycle" problem

---
+++ LeetCode/Linked List/linked-list-cycle.ts
@@ -0,0 +1,17 @@
+// O(n) time | O(1) space
+// n == length of the Linked List
+function hasCycle(head: ListNode | null): boolean {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow!.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
