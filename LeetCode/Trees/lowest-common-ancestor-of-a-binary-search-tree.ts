792bdd84e18b0abb324bc26f91439bd213038259
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-05-17 09:14:07 -0300

    Expanded on the time complexity explanation

--- LeetCode/Trees/lowest-common-ancestor-of-a-binary-search-tree.ts
+++ LeetCode/Trees/lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -12,7 +12,13 @@
  * }
  */
 
-// O(h) time | O(1) space
+
+// Time Complexity: O(h) on average
+//  - Best case: O(log n) when the BST is balanced
+//  - Worst case: O(n) when the BST is a linked list
+//
+// Space Complexity: O(1)
+//
 // h === height of the BST
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
   // Make a reference to the head of the BST
