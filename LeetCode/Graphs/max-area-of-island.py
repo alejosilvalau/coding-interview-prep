991e8d29f9cf4435491a1172a5562608f7137b77
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2025-06-24 06:41:40 -0300

    Finished 'Max Area of Island' problem

--- LeetCode/Graphs/max-area-of-island.py
+++ LeetCode/Graphs/max-area-of-island.py
@@ -1,13 +1,19 @@
  
+from typing import List
+
+# O(r * c) time | O(r * c) space
+# r == number of rows in the grid
+# c == number of columns in the grid
 class Solution:
   def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
     ROWS, COLS = len(grid), len(grid[0])
+    directions = [(1,0), (-1,0), (0,1), (0,-1)]
     visited = set()
     maxArea = 0
 
     def depthFirstSearch(row, col):
       if (
-        row < 0 or r == ROWS or 
+        row < 0 or row == ROWS or 
         col < 0 or col == COLS or 
         grid[row][col] == 0 or 
         (row, col) in visited
@@ -15,11 +21,10 @@ class Solution:
         return 0
 
       visited.add((row, col))
-      return (
-        1 + 
-        depthFirstSearch(row + 1, col) + 
-        // rest
-      )
+      currArea = 1
+      for dr, dc in directions:
+        currArea += depthFirstSearch(row + dr, col + dc)
+      return currArea
 
 
     for r in range(ROWS):
