dc77bac06551818ffdba688c4adcd2ac418f0336
Alejo Silva <alejo.lautaro@hotmail.com>
2025-11-12 10:52:04 -0300

    Implement "Jump Game" solution using greedy technique

---
+++ LeetCode/Greedy/jump-game.py
@@ -0,0 +1,19 @@
+from ast import List
+
+
+# O(n) time | O(1) space
+# n == length of the nums array
+# It uses the greedy technique
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goalIdx = len(nums) - 1
+
+        # Start from the end to the start of the array
+        for i in range(len(nums) - 1, -1, -1):
+            # If the previous index has a sufficient step size, then move the goalIdx backwards
+            if i + nums[i] >= goalIdx:
+                goalIdx = i
+
+        # If the goalIdx is at the start of the array, means that there
+        # is a way to reach the beginning, otherwise there is not
+        return True if goalIdx == 0 else False
