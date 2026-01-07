4e9774d506d99bad9aa5b81e376a1ea613b2b221
Alejo Silva <alejo.lautaro@hotmail.com>
2026-01-07 07:30:46 -0300

    Fix of typo

--- LeetCode/Bit Manipulation/reverse-bits.py
+++ LeetCode/Bit Manipulation/reverse-bits.py
@@ -1,7 +1,7 @@
 """
 # String manipulation solution
 # O(1) time | O(1) space
-# If the size of the int lenght is
+# If the size of the int length is
 # unbounded, then time and space are O(n).
 # However, this problem involves max 32 bits integer
 # Therefore, both are O(1) as we drop the constant
