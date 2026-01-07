c658d71bab64d6c1f6855799acb331cd5db6a385
Alejo Silva <alejo.lautaro@hotmail.com>
2026-01-07 07:29:48 -0300

    First solution for "Reverse Bits" problem

---
+++ LeetCode/Bit Manipulation/reverse-bits.py
@@ -0,0 +1,15 @@
+"""
+# String manipulation solution
+# O(1) time | O(1) space
+# If the size of the int lenght is
+# unbounded, then time and space are O(n).
+# However, this problem involves max 32 bits integer
+# Therefore, both are O(1) as we drop the constant
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        b = bin(n)[2:]
+        rb = b[::-1]
+        mis_zeros = '0' * (32 - len(rb))
+        rb += mis_zeros
+        return int(rb, 2)
+"""
